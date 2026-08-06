49105b7e1204568cb321da3c936496593f64b7b6
Antonio Triana <trianadevjs@gmail.com>
2026-08-06 16:03:24 -0600

    feat(database): integrate prisma with postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
