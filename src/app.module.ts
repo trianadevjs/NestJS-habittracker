2d702cb49c7a293e111dfad443e78a1548f4cbe7
Antonio Triana <trianadevjs@gmail.com>
2026-08-06 19:29:38 -0600

    feat(config): add environment variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+
+        PORT: Joi.number().default(3000),
+
+        NODE_ENV: Joi.string()
+          .valid('development', 'test', 'production')
+          .default('development'),
+      }),
     }),
-    PrismaModule],
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
