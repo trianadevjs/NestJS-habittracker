21cba778ac8ae307a18b0565591fe81d0a7e991c
Antonio Triana <trianadevjs@gmail.com>
2026-08-06 19:56:08 -0600

    chore(joi): resolving joi import conflicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from 'joi';
+import Joi from 'joi';
 
 @Module({
   imports: [
